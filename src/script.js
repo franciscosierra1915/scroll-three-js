a57239226647b522f90e1016514e3ea183cdaac2
Francisco Sierra <64598498+franciscosierra1915@users.noreply.github.com>
2022-03-31 15:28:14 -0500

    done

--- src/script.js
+++ src/script.js
@@ -1,17 +1,5 @@
 import "./style.css";
 import * as THREE from "three";
-import * as dat from "lil-gui";
-
-/**
- * Debug
- */
-const gui = new dat.GUI();
-
-const parameters = {
-  materialColor: "#ffeded",
-};
-
-gui.addColor(parameters, "materialColor");
 
 /**
  * Base
@@ -23,48 +11,34 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 // Texture
-const textureLoader = new THREE.TextureLoader()
-const gradientTexture = textureLoader.load('textures/gradients/3.jpg')
-gradientTexture.magFilter = THREE.NearestFilter
-
+const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load("/textures/matcaps/8.png");
 
 // Material
-const material = new THREE.MeshToonMaterial({
-    color: parameters.materialColor,
-    gradientMap: gradientTexture
-})
-
+const material = new THREE.MeshMatcapMaterial({
+  matcap: matcapTexture,
+});
 
 // Objects
-const objectsDistance = 4
-const mesh1 = new THREE.Mesh(
-    new THREE.TorusGeometry(1, 0.4, 16, 60),
-    material
-)
-const mesh2 = new THREE.Mesh(
-    new THREE.ConeGeometry(1, 2, 32),
-    material
-)
+const objectsDistance = 4;
+const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 64, 64), material);
+const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 128), material);
 const mesh3 = new THREE.Mesh(
-    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
-    material
-)
+  new THREE.TorusKnotGeometry(0.8, 0.35, 200, 16),
+  material
+);
 
-mesh1.position.x = 2
-mesh2.position.x = - 2
-mesh3.position.x = 2
+mesh1.position.x = 2;
+mesh2.position.x = -2;
+mesh3.position.x = 2;
 
-mesh1.position.y = - objectsDistance * 0
-mesh2.position.y = - objectsDistance * 1
-mesh3.position.y = - objectsDistance * 2
+mesh1.position.y = -objectsDistance * 0;
+mesh2.position.y = -objectsDistance * 1;
+mesh3.position.y = -objectsDistance * 2;
 
-scene.add(mesh1, mesh2, mesh3)
-/**
- * Lights
- */
- const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
- directionalLight.position.set(1, 1, 0)
- scene.add(directionalLight)
+scene.add(mesh1, mesh2, mesh3);
+
+const sectionMeshes = [mesh1, mesh2, mesh3];
 
 /**
  * Sizes
@@ -91,6 +65,11 @@ window.addEventListener("resize", () => {
 /**
  * Camera
  */
+
+// Group
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
+
 // Base camera
 const camera = new THREE.PerspectiveCamera(
   35,
@@ -109,14 +88,59 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.setClearAlpha(0)
+renderer.setClearAlpha(0);
+
+/**
+ * Cursor
+ */
+ const cursor = {}
+ cursor.x = 0
+ cursor.y = 0
+
+ window.addEventListener('mousemove', (event) =>
+ {
+     cursor.x = event.clientX / sizes.width - 0.5
+     cursor.y = event.clientY / sizes.height - 0.5
+ 
+     console.log(cursor)
+ })
+
+/**
+ * Scroll
+ */
+ let scrollY = window.scrollY;
+
+ window.addEventListener("scroll", () => {
+   scrollY = window.scrollY;
+ 
+   console.log(scrollY)
+ });
+
 /**
  * Animate
  */
-const clock = new THREE.Clock();
+ const clock = new THREE.Clock()
+ let previousTime = 0
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+
+  // Animate meshes
+  for (const mesh of sectionMeshes) {
+    mesh.rotation.x = elapsedTime * 0.1;
+    mesh.rotation.y = elapsedTime * 0.12;
+  }
+
+  // Animate camera
+  camera.position.y = - scrollY / sizes.height * objectsDistance
+
+  const parallaxX = cursor.x
+  const parallaxY = - cursor.y
+
+  cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+  cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
 
   // Render
   renderer.render(scene, camera);
